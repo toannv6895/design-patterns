d4bec90635f447af50282e3dd9c3a0b871f17b9b
Toan Nguyen <tnguyen@us.maxnerva.com>
2025-03-10 17:30:41 +0700

    lazy singleton

---
+++ Singleton-sample/src/main/java/org/toannguyen/LazySingleton.java
@@ -0,0 +1,15 @@
+package org.toannguyen;
+
+public class LazySingleton {
+    private static LazySingleton instance;
+
+    private LazySingleton() {}
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            instance = new LazySingleton();
+        }
+
+        return instance;
+    }
+}
