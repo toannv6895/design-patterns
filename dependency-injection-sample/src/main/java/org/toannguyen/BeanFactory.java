07340662b152eaed4bb845b8e23f6d667643524d
Toan Nguyen <tnguyen@us.maxnerva.com>
2025-03-17 11:01:51 +0700

    refactor BeanFactory to easy extends

--- dependency-injection-sample/src/main/java/org/toannguyen/BeanFactory.java
+++ dependency-injection-sample/src/main/java/org/toannguyen/BeanFactory.java
@@ -5,14 +5,18 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public enum BeanFactory {
-    INSTANCE;
-    public <T> T getInstanceOf(Class<T> clazz, Object... args) {
-        try {
-            Class<?>[] argsClasses = Arrays.stream(args).map(Object::getClass).toArray(Class<?>[]::new);
-            Constructor<T> constructor = clazz.getConstructor(argsClasses);
-            return constructor.newInstance(argsClasses);
-        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException(e);
+    INSTANCE {
+        @Override
+        public <T> T getInstanceOf(Class<T> clazz, Object... args) {
+            try {
+                Class<?>[] argsClasses = Arrays.stream(args).map(Object::getClass).toArray(Class<?>[]::new);
+                Constructor<T> constructor = clazz.getConstructor(argsClasses);
+                return constructor.newInstance(args);
+            } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
+    };
+
+    public abstract <T> T getInstanceOf(Class<T> clazz, Object... args);
 }
